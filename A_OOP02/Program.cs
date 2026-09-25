bf96e343f06b804ba2a69f0adb837182beb3f039
hamza-salah12 <privatew828@gmail.com>
2026-09-25 17:58:44 +0300

    DeliveryAddress

--- A_OOP02/Program.cs
+++ A_OOP02/Program.cs
@@ -2,29 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
 
-            #region part 1
-            // What is the difference between a class and a struct :
-            //  class is a reference type, while a struct is a value type.
-            //  This means that when you create an instance of a class, it is stored on the heap and accessed through a reference, while an instance of a struct is stored on the stack and accessed directly.
-            ///-------------------------------------------------------------------------------------------------------------------
-            // Why are classes more suitable than structs for large applications?: 
-            //  Classes are more suitable for large applications because they can be easily extended and modified through inheritance and polymorphism, while structs are limited in their functionality and cannot be inherited from.
-            ///--------------------------------------------------------------------------------------------------------------------
-            // Question 2 
-            // a) Which class is the parent class? >> Shipment
-            // b) Which class is the child class? >> ExpressShipment
-            // c) What members are inherited by ExpressShipment? >> TrackingCode
-            // d) Why is inheritance better than duplicating the same code in multiple classes? >> apply to prinsiple reused code and decrease the repetit anf minantance of the code
-            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        #region part 1
+        // What is the difference between a class and a struct :
+        //  class is a reference type, while a struct is a value type.
+        //  This means that when you create an instance of a class, it is stored on the heap and accessed through a reference, while an instance of a struct is stored on the stack and accessed directly.
+        ///-------------------------------------------------------------------------------------------------------------------
+        // Why are classes more suitable than structs for large applications?: 
+        //  Classes are more suitable for large applications because they can be easily extended and modified through inheritance and polymorphism, while structs are limited in their functionality and cannot be inherited from.
+        ///--------------------------------------------------------------------------------------------------------------------
+        // Question 2 
+        // a) Which class is the parent class? >> Shipment
+        // b) Which class is the child class? >> ExpressShipment
+        // c) What members are inherited by ExpressShipment? >> TrackingCode
+        // d) Why is inheritance better than duplicating the same code in multiple classes? >> apply to prinsiple reused code and decrease the repetit anf minantance of the code
+        //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-            #endregion
+        #endregion
 
 
-
-
-        }
     }
 }
